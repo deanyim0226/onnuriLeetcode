fd8c2e39bd79f2f3ea886d4ef6f97b39665c1e7c
ChaesunYim <97508288+ChaesunYim@users.noreply.github.com>
2024-01-11 22:49:33 -0800

    delete a wrong solution

---
+++ solutions/1.twosum/sunny.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int, int> number;
+
+        for(int i = 0; i < nums.size(); i++){
+
+            int n = target - nums[i];
+
+            if(number.find(n) != number.end()){
+
+                return {twosum [n], i};
+            }
+            
+            number[nums[i]] = i;
+           
+
+        }
+
+        return {};
+    }
+    
+};
