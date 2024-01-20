b8ba33fe9e18e290c2dbdefca46bfa9b602413d5
YusunL <47683668+YusunL@users.noreply.github.com>
2024-01-20 09:50:17 -0800

    Add files via upload

---
+++ solutions/49.GroupAnagrams/yusunl.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution{
+public:
+    vector<vector<string>> groupAnagrams(vector<string>&strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, int> hash;
+        for (int i = 0; i < strs.size(); i++) {
+            string tmp = strs[i];
+            sort(tmp.begin(), tmp.end());
+            if (hash.find(tmp) != hash.end()) {
+                ans[hash[tmp]].push_back(strs[i]);
+            }
+            else {
+                vector<string> str = { strs[i],};
+                hash[tmp] = ans.size();
+                ans.push_back(str);
+            }
+        }
+        return ans;
+    }
+};
