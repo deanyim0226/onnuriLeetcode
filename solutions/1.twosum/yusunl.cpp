aa4232d177677ecb01f0ad482f18e5ce1c96cc71
YusunL <47683668+YusunL@users.noreply.github.com>
2024-01-20 09:56:46 -0800

    Add files via upload

---
+++ solutions/1.twosum/yusunl.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int, int> hash;
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            if (hash.find(target - nums[i]) == hash.end()) hash[nums[i]] = i;
+            else {
+                ans.push_back(i);
+                ans.push_back(hash[target - nums[i]]);
+                break;
+            }
+        }
+        return ans;
+    }
+};
