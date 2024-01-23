a5a3ea1db875f4c11aa84c1ab5cf84f4e84c556b
Hyo Kim <hyo.kim968@gmail.com>
2024-01-23 14:17:20 -0800

    add recursive solution

--- solutions/118.Pascal'sTriangle/hyo.js
+++ solutions/118.Pascal'sTriangle/hyo.js
@@ -1,3 +1,4 @@
+// ITERATIVE
 var generate = function (numRows) {
   let result = [[1]];
 
@@ -18,6 +19,23 @@ var generate = function (numRows) {
     newRow.push(1);
     result.push(newRow);
   }
+
+  return result;
+};
+
+// RECURSION
+var generate_recursive = function (numRows) {
+  if (numRows === 1) return [[1]];
+
+  let result = generate_recursive(numRows - 1);
+  let previousRow = result[result.length - 1];
+  let newRow = [1];
+
+  for (let i = 0; i < previousRow.length - 1; i++) {
+    newRow.push(previousRow[i] + previousRow[i + 1]);
+  }
+  newRow.push(1);
+  result.push(newRow);
 
   return result;
 };
