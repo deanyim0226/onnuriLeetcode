aaf02edfaf704cccc4a5c0f6e8db940b31a2c787
ChaesunYim <97508288+ChaesunYim@users.noreply.github.com>
2024-02-07 21:12:07 -0800

    recursion

---
+++ solutions/231.PowerOfTwo/sunny.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+    public:
+        
+    bool helper(int n){
+
+        if(n==1){
+
+            return true;  //base case
+        }
+
+        if(n <= 0 || n%2 != 0){
+
+            return false;
+        }
+
+        return helper(n/2);
+        
+    }
+
+        
+    
+    bool isPowerOfTwo(int n) {
+
+        return helper(n);
+        
+    }
+
+    
+   
+};
