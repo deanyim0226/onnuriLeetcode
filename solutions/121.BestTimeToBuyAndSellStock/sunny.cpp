28f74dc0f37a921006d239ab23b4c408cc87c592
ChaesunYim <97508288+ChaesunYim@users.noreply.github.com>
2024-02-07 21:16:54 -0800

    week4

---
+++ solutions/121.BestTimeToBuyAndSellStock/sunny.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+      int n = prices.size();
+
+      int minprice = prices[0];
+
+      int maxprof = 0;
+
+      if(n<=1){
+
+          return 0;
+      }
+
+
+      for(int i=1; i<n; i++){
+
+          if(prices[i] < minprice){
+
+              minprice = prices[i];
+          }
+
+          int currentprof = prices[i]-minprice;
+
+
+          if(currentprof > maxprof){
+
+              maxprof = currentprof;
+          }
+
+      }
+
+
+      return maxprof;
+
+    }
+};
